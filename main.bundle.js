76d91a14483d52b6964e7902f57185661187638d
Charlotte Moore <mooc123@gmail.com>
2017-05-11 14:37:02 -0600

    ran build

--- main.bundle.js
+++ main.bundle.js
@@ -53,6 +53,8 @@
 	    Food.remove();
 	  });
 
+	  $('#new-food').submit(event => Food.addFood());
+
 	  $('form').on('submit', event => event.preventDefault());
 	});
 
@@ -61,13 +63,10 @@
 /***/ (function(module, exports, __webpack_require__) {
 
 	const API = 'https://qs-be.herokuapp.com/api/v1/';
-	// const LOCAL ='http://localhost:3000/api/v1/foods'
-
+	const LOCAL = 'http://localhost:3000/api/v1/';
 	$ = __webpack_require__(2);
 
 	const Food = class Food {
-
-	  constructor() {}
 
 	  getAll() {
 	    return $.ajax({
@@ -79,21 +78,72 @@
 	  }
 
 	  remove() {
-	    alert("I did it");
+	    const button = event.target;
+	    const id = $(button).data("food-id");
+
+	    return $.ajax({
+	      url: API + `foods/${id}`,
+	      method: 'DELETE'
+	    }).done(removeFromFoodTable(button)).fail(error => {
+	      console.error(error);
+	    });
+	  }
+
+	  addFood() {
+	    let form = $('#new-food');
+	    let newFood = {};
+	    newFood['name'] = $("input[name=food-name]").val();
+	    newFood['calories'] = $("input[name=food-calories]").val();
+	    $('.calorie-error').empty().remove();
+	    $('.name-error').empty().remove();
+
+	    if (newFood.name == "" && newFood.calories == "") {
+	      caloriesError();
+	      nameError();
+	    } else if (newFood.calories == "") {
+	      caloriesError();
+	    } else if (newFood.name == "") {
+	      nameError();
+	    } else {
+	      return $.ajax({
+	        url: API + 'foods',
+	        method: 'POST',
+	        data: newFood
+	      }).done(addToFoodTable).fail(error => {
+	        console.error(error);
+	      });
+	    };
 	  }
 	};
 
 	function makeFoodTable(data) {
 	  data.forEach(food => {
-	    $('.foods tbody').append(`<tr><td>${food.name}</td><td>${food.calories}</td><td><button class='remove'>-</button></td></tr>`);
+	    $('.foods tbody').prepend(`<tr><td >${food.name}</td><td>${food.calories}</td><td><button data-food-id=${food.id} class='remove btn red darken-3'>-</button></td></tr>`);
 	  });
 	}
 
-	// $(document).ready(function(){
-	//   debugger
-	//   let food = new Food
-	//   food.getAll()
-	// });
+	function addToFoodTable(data) {
+	  const food = data[0];
+	  $(`<tr><td>${food.name}</td><td>${food.calories}</td><td><button data-food-id=${food.id} class='remove btn red darken-3'>-</button></td></tr>`).prependTo('.foods tbody');
+	  $('.calorie-error').empty().remove();
+	  $('.name-error').empty().remove();
+	  $('input[name=food-name]').val('');
+	  $('input[name=food-calories]').val('');
+	}
+
+	function removeFromFoodTable(button) {
+	  $(button).parent().parent().empty().remove();
+	}
+
+	function caloriesError() {
+	  $('.calorie-error').empty().remove();
+	  $('#calorie-field').parent().after(`<div class='calorie-error'>Please enter a calorie amount</div>`);
+	}
+	function nameError() {
+	  $('.name-error').empty().remove();
+	  $('#name-field').parent().after(`<div class='name-error'>Please enter a food name
+	</divl>`);
+	}
 
 	module.exports = new Food();
 
