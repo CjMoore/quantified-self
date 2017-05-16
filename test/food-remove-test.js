5348993c6599b9652ae1df3d39c6886bbd146a31
Charlotte Moore <mooc123@gmail.com>
2017-05-16 14:28:52 -0600

    extracted add food and get all foods

---
+++ test/food-remove-test.js
@@ -0,0 +1,9 @@
+const assert = require('chai').assert
+const FoodRemove = require('../lib/food-remove')
+const $ = require('jquery')
+
+describe ( 'RemoveFood', () => {
+  it('is removes food from table',  (done) => {
+    
+  })
+})
